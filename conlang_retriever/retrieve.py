7beb3f6cccfe59f5cec10bf77c5c256dfa899574
Nicolas Antonio Cloutier Arce <nicocloutier1@gmail.com>
2024-01-12 11:16:43 -0500

    connect to database

--- conlang_retriever/retrieve.py
+++ conlang_retriever/retrieve.py
@@ -1,14 +1,14 @@
-import requests, bs4, io
+import requests, bs4, io, sqlite3
 import pandas as pd
 
 URL = 'https://database.conlang.org/search/'
 
-def make_df(url:str=URL) -> pd.DataFrame:
+def make_df(url: str=URL) -> pd.DataFrame:
     '''
     Make a pandas dataframe from a webpage with an HTML table.
     Arguments:
         Optional:
-        `url:str=URL`: the url to request from, conlang database by default.
+        `url: str=URL`: the url to request from, conlang database by default.
     Returns:
         `pd.DataFrame`: the dataframe made from the table.
     '''
@@ -17,3 +17,26 @@ def make_df(url:str=URL) -> pd.DataFrame:
     table = soup.find('table')
     df = pd.read_html(str(table))[0].drop('Unnamed: 0', axis=1)
     return df.drop('Unnamed: 0', axis=1) if 'Unnamed: 0' in df.columns else df
+
+def convert_row(row: tuple[int, pd.Series]) -> str:
+    '''
+    Convert row in a DataFrame to a string for insertion in a SQLite database.
+    Arguments:
+        `row: tuple[int, pd.Series]`: an item in the generator produced by the `.iterrows()` method on a DataFrame.
+    Returns:
+        `str`: the row to be inserted.
+    '''
+    return str(tuple(row[1].values))
+
+def convert_db(df: pd.DataFrame) -> None:
+    '''
+    Convert pandas DataFrame to SQLite database and write to file.
+    Arguments:
+        `df: pd.DataFrame`: the DataFrame to convert.
+    Returns:
+        `None`
+    '''
+    connection = sqlite3.connect('../data/conlangs.db')
+    cursor = connection.cursor()
+    cursor.execute(f'CREATE TABLE conlangs({", ".join(df.columns.values)})')
+    cursor.execute(f'INSERT INTO conlangs VALUES {", ".join(convert_row(row) for row in df.iterrows())}')
