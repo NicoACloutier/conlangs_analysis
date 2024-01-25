bd41e36d19a6184a98e26b78eba5a663f0c139ed
Nicolas Antonio Cloutier Arce <nicocloutier1@gmail.com>
2024-01-24 20:39:03 -0500

    add dashboard

--- run_viz.py
+++ run_viz.py
@@ -1,7 +1,11 @@
 from conlang_retriever import retrieve
+import dash, pandas as pd
 
 def main():
     retrieve.convert_db(retrieve.make_df(retrieve.URL))
+    app = dash.Dash(__name__)
+    app.layout = dash.html.Div([])
+    app.run(debug=True)
 
 if __name__ == '__main__':
     main()
