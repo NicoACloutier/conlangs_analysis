b36a65dbd835dcb743bc72e4bbffbfbe5966dd20
Nicolas Antonio Cloutier Arce <nicocloutier1@gmail.com>
2024-01-29 20:54:26 -0500

    add time histogram

--- run_viz.py
+++ run_viz.py
@@ -1,5 +1,9 @@
 from conlang_retriever import retrieve
-import dash, plotly, sqlite3, pyspark, pandas as pd
+import plotly.express as px
+import dash, plotly, sqlite3, pyspark, findspark, pandas as pd
+
+MINIMUM_YEAR = 1000 # the minimum year for included conlangs in the time histogram
+YEAR_COLUMN = 'start_year' # the column of the database with year information
 
 def read_database(spark: pyspark.sql.SparkSession) -> pyspark.sql.DataFrame:
     '''
@@ -11,17 +15,49 @@ def read_database(spark: pyspark.sql.SparkSession) -> pyspark.sql.DataFrame:
     '''
     connection = sqlite3.connect(retrieve.DB_LOCATION)
     cursor = connection.cursor()
-    data = cursor.execute('SELECT * FROM conlangs;').fetchall()
-    columns = [column[1] for column in cursor.execute('PRAGMA table_info(conlangs);').fetchall()]
+    data = cursor.execute('SELECT * FROM conlangs;').fetchall() # select all data from the `conlangs` table in the scraped database
+    columns = [column[1] for column in cursor.execute('PRAGMA table_info(conlangs);').fetchall()] # find all column names for the `conlangs` table
     return spark.createDataFrame(data, schema=columns)
 
+def make_time_histogram(df: pyspark.sql.DataFrame, column: str=YEAR_COLUMN, minimum_year: int=MINIMUM_YEAR) -> plotly.graph_objects.Figure:
+    '''
+    Make a timeline histogram from the time data.
+    Arguments:
+        `df: pyspark.sql.DataFrame`: the PySpark dataframe utilized to create the histogram.
+        Optional:
+        `column: str=YEAR_COLUMN`: the name of the column containing start years for conlangs.
+        `minimum_year: int=MINIMUM_YEAR`: the minimum year to be included in the histogram.
+    Returns:
+        `plotly.graph_objects.Figure`: a plotly `Figure` object to be included in the dashboard.
+    '''
+    df = df.filter(df[column] != '')
+    int_udf = pyspark.sql.functions.udf(lambda x: int(x), pyspark.sql.types.IntegerType())   
+    df.withColumn(column, int_udf(pyspark.sql.functions.col(column)))
+    df = df.filter(df[column] > minimum_year)
+    return px.histogram(df, x=column)
+
 def main():
+    findspark.init()
     retrieve.convert_db(retrieve.make_df(retrieve.URL)) # retrieve data and make database if necessary
     spark = pyspark.sql.SparkSession.builder.getOrCreate()
     data = read_database(spark)
     
+    # create visualizations
+    time_histogram = make_time_histogram(data)
+    
     app = dash.Dash(__name__)
-    app.layout = dash.html.Div([])
+    app.layout = dash.html.Div(children=[
+        dash.html.H1(children='Conlangs database analysis'),
+
+        dash.html.Div(children='''
+            An analysis of the online and freely available conlangs database.
+        '''),
+
+        dash.dcc.Graph(
+            id='time_histogram',
+            figure=time_histogram
+        )
+    ])
     app.run(debug=True)
 
 if __name__ == '__main__':
